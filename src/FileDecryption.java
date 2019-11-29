023c357cb17ecdf942110589dc096d1cccf9643d
CheatCod <2892084432j@gmail.com>
2019-11-29 13:49:40 -0500

    fixed up stuff?

--- src/FileDecryption.java
+++ src/FileDecryption.java
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
@@ -7,7 +8,6 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
-
 public class FileDecryption {
 	public static void main(String[] args) throws Exception {
 
@@ -42,49 +42,4 @@ public class FileDecryption {
 		fos.flush();
 		fos.close();
 	}
-=======
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
-
-public class FileDecryption {
-	public static void main(String[] args) throws Exception {
-
-		String password = "12345";
-		PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray());
-		SecretKeyFactory secretKeyFactory = SecretKeyFactory
-				.getInstance("PBEWithMD5AndTripleDES");
-		SecretKey secretKey = secretKeyFactory.generateSecret(pbeKeySpec);
-
-		FileInputStream fis = new FileInputStream("F:\\Eclipse Photon\\Encryption\\bin\\plainfile.des");
-		byte[] salt = new byte[8];
-		fis.read(salt);
-
-		PBEParameterSpec pbeParameterSpec = new PBEParameterSpec(salt, 100);
-
-		Cipher cipher = Cipher.getInstance("PBEWithMD5AndTripleDES");
-		cipher.init(Cipher.DECRYPT_MODE, secretKey, pbeParameterSpec);
-		FileOutputStream fos = new FileOutputStream("F:\\Eclipse Photon\\Encryption\\bin\\plainfile_decrypted.txt");
-		byte[] in = new byte[64];
-		int read;
-		while ((read = fis.read(in)) != -1) {
-			byte[] output = cipher.update(in, 0, read);
-			if (output != null)
-				fos.write(output);
-		}
-
-		byte[] output = cipher.doFinal();
-		if (output != null)
-			fos.write(output);
-
-		fis.close();
-		fos.flush();
-		fos.close();
-	}
->>>>>>> 3a9fe043f1f728db036dcb4b75b5b7ac0ca3e141
 }
--- src/FileEncryption.java
+++ src/FileEncryption.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Random;
@@ -48,55 +48,4 @@ public class FileEncryption {
 		outFile.close();
 	}
 
-=======
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.Random;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
-
-public class FileEncryption {
-
-	public static void main(String[] args) throws Exception {
-
-		FileInputStream inFile = new FileInputStream("F:\\Eclipse Photon\\Encryption\\bin\\plainfile.txt");
-		FileOutputStream outFile = new FileOutputStream("F:\\Eclipse Photon\\Encryption\\bin\\plainfile.des");
-
-		String password = "12345";
-		PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray());
-		SecretKeyFactory secretKeyFactory = SecretKeyFactory
-				.getInstance("PBEWithMD5AndTripleDES");
-		SecretKey secretKey = secretKeyFactory.generateSecret(pbeKeySpec);
-
-		byte[] salt = new byte[8];
-		Random random = new Random();
-		random.nextBytes(salt);
-
-		PBEParameterSpec pbeParameterSpec = new PBEParameterSpec(salt, 100);
-		Cipher cipher = Cipher.getInstance("PBEWithMD5AndTripleDES");
-		cipher.init(Cipher.ENCRYPT_MODE, secretKey, pbeParameterSpec);
-		outFile.write(salt);
-
-		byte[] input = new byte[64];
-		int bytesRead;
-		while ((bytesRead = inFile.read(input)) != -1) {
-			byte[] output = cipher.update(input, 0, bytesRead);
-			if (output != null)
-				outFile.write(output);
-		}
-
-		byte[] output = cipher.doFinal();
-		if (output != null)
-			outFile.write(output);
-
-		inFile.close();
-		outFile.flush();
-		outFile.close();
-	}
-
->>>>>>> 3a9fe043f1f728db036dcb4b75b5b7ac0ca3e141
 }
